211a353b6b4cf519ddc152a5ccafb994d9f53419
Fernando Belotto <fernando.bbosco@gmail.com>
2022-12-21 15:26:29 -0300

    Initial Commit

---
+++ SwiftCrud/SwiftCrudApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCrudApp.swift
+//  SwiftCrud
+//
+//  Created by Fernando Bosco on 21/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCrudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
